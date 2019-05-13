0e888722118f58cf06a1502fa85c130baa6402ca
Ahmad Alsodani <ahmad.alsodani@hyperionstudios.com.au>
2019-05-13 23:11:25 +0800

    add whats new screen

--- lib/screens/home_tabs/whats_new.dart
+++ lib/screens/home_tabs/whats_new.dart
@@ -8,8 +8,236 @@ class WhatsNew extends StatefulWidget {
 class _WhatsNewState extends State<WhatsNew> {
   @override
   Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _drawHeader(),
+          _drawTopStories(),
+        ],
+      ),
+    );
+  }
+
+  Widget _drawHeader() {
+    TextStyle _headerTitle = TextStyle(
+        color: Colors.white, fontSize: 22, fontWeight: FontWeight.w600);
+    TextStyle _headerDescription = TextStyle(
+      color: Colors.white,
+      fontSize: 18,
+    );
+
     return Container(
-      color: Colors.teal,
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.25,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: ExactAssetImage('assets/images/placeholder_bg.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 48, right: 48),
+              child: Text(
+                'How Terriers & Royals Gatecrashed Final',
+                style: _headerTitle,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 34, right: 34),
+              child: Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.',
+                style: _headerDescription,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _drawTopStories() {
+    return Container(
+      color: Colors.grey.shade100,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 16, top: 16),
+            child: _drawSectionTitle('Top Stories'),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Card(
+              child: Column(
+                children: <Widget>[
+                  _drawSingleRow(),
+                  _drawDivider(),
+                  _drawSingleRow(),
+                  _drawDivider(),
+                  _drawSingleRow(),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 16, bottom: 8, top: 8),
+                  child: _drawSectionTitle('Recent Updates'),
+                ),
+                _drawRecentUpdatesCard(Colors.deepOrange),
+                _drawRecentUpdatesCard(Colors.teal),
+                SizedBox(
+                  height: 48,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _drawDivider() {
+    return Container(
+      height: 1,
+      width: double.infinity,
+      color: Colors.grey.shade100,
+    );
+  }
+
+  Widget _drawSingleRow() {
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          SizedBox(
+            child: Image(
+              image: ExactAssetImage('assets/images/placeholder_bg.png'),
+              fit: BoxFit.cover,
+            ),
+            width: 124,
+            height: 124,
+          ),
+          SizedBox(
+            width: 16,
+          ),
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'The World Global Warming Annual Summit',
+                  maxLines: 2,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 18,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('Michael Adams'),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.timer),
+                        Text('15 min'),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _drawSectionTitle(String title) {
+    return Text(
+      title,
+      style: TextStyle(
+          color: Colors.grey.shade700,
+          fontWeight: FontWeight.w600,
+          fontSize: 16),
+    );
+  }
+
+  Widget _drawRecentUpdatesCard(Color color) {
+    return Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: ExactAssetImage('assets/images/placeholder_bg.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * 0.25,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16, left: 16),
+            child: Container(
+              padding: EdgeInsets.only(left: 24, right: 24, top: 2, bottom: 2),
+              decoration: BoxDecoration(
+                color: color,
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: Text(
+                'SPORT',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 8),
+            child: Text(
+              'Vettel is Ferrari Number One - Hamilton',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 16),
+            child: Row(
+              children: <Widget>[
+                Icon(
+                  Icons.timer,
+                  color: Colors.grey,
+                  size: 18,
+                ),
+                SizedBox( width: 4, ),
+                Text(
+                  '15 Min',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
