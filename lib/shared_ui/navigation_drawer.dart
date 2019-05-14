039b0eaa8538761c0b89ce1b22b04f441d41c63a
Ahmad Alsodani <ahmad.alsodani@hyperionstudios.com.au>
2019-05-14 23:02:41 +0800

    add nav menu base

--- lib/shared_ui/navigation_drawer.dart
+++ lib/shared_ui/navigation_drawer.dart
@@ -6,8 +6,41 @@ class NavigationDrawer extends StatefulWidget {
 }
 
 class _NavigationDrawerState extends State<NavigationDrawer> {
+  List<String> navMenu = [
+    'Explore',
+    'Headline News',
+    'Read Later',
+    'Videos',
+    'Photos',
+    'Settings',
+    'Logout'
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: Padding(
+        padding: EdgeInsets.only(top: 75, left: 24),
+        child: ListView.builder(
+          itemBuilder: (context, position) {
+            return Padding(
+              padding: const EdgeInsets.all(8),
+              child: ListTile(
+                title: Text(
+                  navMenu[position],
+                  style: TextStyle(color: Colors.grey.shade700, fontSize: 22),
+                ),
+                trailing: Icon(
+                  Icons.chevron_right,
+                  color: Colors.grey.shade400,
+                ),
+                onTap: () {},
+              ),
+            );
+          },
+          itemCount: navMenu.length,
+        ),
+      ),
+    );
   }
 }
