d7ba09b718aca61df5ffa557a6c4f5a1bbea5e3f
Ahmad Alsodani <ahmad.alsodani@hyperionstudios.com.au>
2019-05-16 22:15:43 +0800

    finish twitter feed screen

--- lib/screens/twitter_feed.dart
+++ lib/screens/twitter_feed.dart
@@ -19,14 +19,19 @@ class _TwitterFeedState extends State<TwitterFeed> {
       ),
       drawer: NavigationDrawer(),
       body: ListView.builder(
+        padding: EdgeInsets.all( 8 ),
         itemBuilder: (context, position) {
-          return Card(
-            child: Column(
-              children: <Widget>[
-                _cardHeader(),
-                _cardBody(),
-                _cardFooter(),
-              ],
+          return Padding(
+            padding: const EdgeInsets.only( bottom: 8 ),
+            child: Card(
+              child: Column(
+                children: <Widget>[
+                  _cardHeader(),
+                  _cardBody(),
+                  _drawLine(),
+                  _cardFooter(),
+                ],
+              ),
             ),
           );
         },
@@ -41,7 +46,8 @@ class _TwitterFeedState extends State<TwitterFeed> {
         Padding(
           padding: const EdgeInsets.all(16),
           child: CircleAvatar(
-            backgroundImage: ExactAssetImage('assets/images/placeholder_bg.png'),
+            backgroundImage:
+                ExactAssetImage('assets/images/placeholder_bg.png'),
             radius: 24,
           ),
         ),
@@ -50,12 +56,32 @@ class _TwitterFeedState extends State<TwitterFeed> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Text( 'Christina Meyers' ),
-                Text( '@ch_meyers' ),
+                Text(
+                  'Christina Meyers',
+                  style: TextStyle(
+                    color: Colors.grey.shade900,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Text(
+                  '@ch_meyers',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
               ],
             ),
-            SizedBox( height: 8, ),
-            Text( 'Fri, 12 May 2017 • 14.30' ),
+            SizedBox(
+              height: 8,
+            ),
+            Text(
+              'Fri, 12 May 2017 • 14.30',
+              style: TextStyle(color: Colors.grey),
+            ),
           ],
         ),
       ],
@@ -64,8 +90,10 @@ class _TwitterFeedState extends State<TwitterFeed> {
 
   Widget _cardBody() {
     return Padding(
-      padding: const EdgeInsets.only( left: 16, right: 16 , bottom: 8 ),
-      child: Text( 'We also talk about the future of work as the robots advance, and we ask whether a retro phone' ),
+      padding: const EdgeInsets.only(left: 16, right: 16, bottom: 8),
+      child: Text(
+          'We also talk about the future of work as the robots advance, and we ask whether a retro phone' ,
+      style: TextStyle( fontSize: 16 , height: 1.2 , color: Colors.grey.shade900 ),),
     );
   }
 
@@ -77,18 +105,48 @@ class _TwitterFeedState extends State<TwitterFeed> {
         children: <Widget>[
           Row(
             children: <Widget>[
-              IconButton(icon: Icon( Icons.repeat ), color: Colors.orange ,onPressed: (){}),
-              Text( '25' )
+              IconButton(
+                  icon: Icon(
+                    Icons.repeat,
+                    size: 28,
+                  ),
+                  color: Colors.orange,
+                  onPressed: () {}),
+              Text(
+                '25',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16,
+                ),
+              )
             ],
           ),
           Row(
             children: <Widget>[
-              FlatButton(onPressed: (){}, child: Text('SHARE' , style: TextStyle( color: Colors.orange ),)),
-              FlatButton(onPressed: (){}, child: Text('OPEN' , style: TextStyle( color: Colors.orange) , )),
+              FlatButton(
+                  onPressed: () {},
+                  child: Text(
+                    'SHARE',
+                    style: TextStyle(color: Colors.orange),
+                  )),
+              FlatButton(
+                  onPressed: () {},
+                  child: Text(
+                    'OPEN',
+                    style: TextStyle(color: Colors.orange),
+                  )),
             ],
           ),
         ],
       ),
+    );
+  }
+
+  Widget _drawLine() {
+    return Container(
+      height: 1,
+      color: Colors.grey.shade200,
+      margin: EdgeInsets.only( top: 16 ),
     );
   }
 }
